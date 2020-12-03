f120c6d3d484f525f0eab465632fc671d73ffb68
Timo Janzon <67964706+tmjnzn@users.noreply.github.com>
2020-12-03 22:12:16 +0100

    Add files via upload

--- website/js/main.js
+++ website/js/main.js
@@ -1,3 +1,13 @@
+
+/*fensterhöne dynamisch*/
+
+let vh = window.innerHeight * 0.01;
+document.documentElement.style.setProperty('--vh', `${vh}px`);
+
+
+
+
+
 /*menu auf / zu*/
 
 var checkboxMsg = false;
